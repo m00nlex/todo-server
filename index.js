64aea70e08a4e2ba9e3744e1bc3b26aba1c9f1a0
m00nlex <you@example.com>
2023-06-10 21:42:10 +0500

    db connect

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 require('dotenv').config()
-
+const router = require('./routes/indexRouter.js')
+const sequelize = require('./db.js')
+const models = require('./models/models.js')
 const  PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.get('/', (req, res) => res.send('hey gey'))
+app.use('/api', router)
 
-app.listen(PORT, () => console.log("server started on port " + PORT))
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log("server started on port " + PORT))
+    } catch (e){
+        console.log(e);
+    }
+}
+
+start()
