8733d9d17d4ca92529d45d1ca4a7b6698306472a
m00nlex <you@example.com>
2023-07-16 20:32:34 +0500

    notices

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -1,7 +1,7 @@
 const {Task} = require("../models/models");
 
 class TaskController {
-    async getOneTask(req, res){
+    async getOneTask(req, res){ //../api/tasks/1
         const taskId = req.params.id
         const task = await Task.findByPk(taskId)
         res.json(task)
@@ -11,15 +11,18 @@ class TaskController {
         return res.json(tasks)
     }
     async addNewTask(req, res){
-        const {name} = req.body
-        const task = await Task.create({name})
-        return res.json({name})
+        const {name} = req.body                          // {
+        const task = await Task.create({name})     //   "name": "tik-tak"
+        return res.json({name})                          // }
     }
     async deleteTask(req, res){
-        const {id} = req.body
+        const {id} = req.body // example in addNewTask
         const task = await Task.destroy({id})
         return res.json({id})
     } //!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
 module.exports =  new TaskController()
+
+//сделать защиту от дурака, чтобы приложение не крашалось от неправильного ввода
+//сделать защиту от удаления не-админами
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -3,7 +3,7 @@ const router = new Router()
 const TaskController = require('../controllers/task.controllers.js')
 const {Task} = require("../models/models");
 
-router.get('/:id', TaskController.getOneTask)
+router.get('/:id', TaskController.getOneTask) //  ../api/tasks/1
 router.get('/', TaskController.getTasks)
 router.post('/', TaskController.addNewTask)
 router.delete('/:id', TaskController.deleteTask)
